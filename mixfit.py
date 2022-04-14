e57c320a4a2d4b65d96eed111dc6f68d3a7e0366
jakezetci <cosb.off@gmail.com>
2022-04-14 19:00:46 +0300

    v0.1 works only for mu

--- mixfit.py
+++ mixfit.py
@@ -1,9 +1,75 @@
 #!/usr/bin/env python3
-
+import numpy as np
+from scipy.optimize import minimize, Bounds
 
 def max_likelihood(x, tau, mu1, sigma1, mu2, sigma2, rtol=1e-3):
-    pass
+    """
 
+    Args:
+        x (array): массив данных.
+        tau (TYPE): DESCRIPTION.
+        mu1 (TYPE): DESCRIPTION.
+        sigma1 (TYPE): DESCRIPTION.
+        mu2 (TYPE): DESCRIPTION.
+        sigma2 (TYPE): DESCRIPTION.
+        rtol (TYPE, optional): DESCRIPTION. Defaults to 1e-3.
+
+    Returns:
+        None.
+
+    """
+    def p(x, mu, sigma):  # per one x
+        a = np.exp(-(x-mu)**2/(2*sigma**2))
+        return a*(np.pi)**(1/2)/sigma
+
+    def log_p(x, mu, sigma):
+        return -(1/2)*(np.log(sigma**2)+1/sigma**2 * (x-mu)**2)
+
+    def t(x, tau, mu1, mu2, sigma12, sigma22):
+        tau0 = tau
+        tau1 = 1 - tau0
+        
+        T0 =(tau0/ np.sqrt(2*np.pi*sigma12))* np.exp(-0.5*(x-mu1)**2/sigma12)
+        T1 =(tau1/ np.sqrt(2*np.pi*sigma22))* np.exp(-0.5*(x-mu2)**2/sigma22)
+        
+        T_sum = T1 +T0
+        T0 = np.divide(T0, T_sum, out = np.full_like(T0, 0.5), where=T_sum!=0.0)
+        T1 = np.divide(T1, T_sum, out = np.full_like(T1, 0.5), where=T_sum!=0.0)
+        return T0, T1
+
+    def L_to_minimize(th, x):
+        tau, mu1, sigma1, mu2, sigma2 = th
+        th1 = (tau, mu1, sigma1)
+        th2 = (1 - tau, mu2, sigma2)
+        T0, T1 = t(x, *th)
+        L1 = np.sum(T0 * np.log(tau)) + np.sum(
+            T0*log_p(x, mu1, sigma1))
+        L2 = np.sum(T0 * np.log(1 - tau)) + np.sum(
+            T1 *log_p(x, mu2, sigma2))
+        return -L1-L2
+    
+    def jac_L(th, x):
+        tau, mu1, sigma1, mu2, sigma2 = th
+        th1 = (tau, mu1, sigma1)
+        th2 = (1 - tau, mu2, sigma2)
+        T0, T1 = t(x, *th)
+        dtau = np.sum(T0 * (1/tau + np.log(tau)/tau)) + np.sum(
+            T0 * log_p(x, mu1, sigma1) / tau) - np.sum(
+                T1-np.log(1-tau))/(1-tau) - np.sum(
+            T1*log_p(x, mu2, sigma2)) / (1-tau)
+        dmu1 = np.sum(T0 * 1/sigma1**2 * (x-mu1))
+        dsigma1 = np.sum(T0 * (- 1/sigma1 + 1/(sigma1**3) * log_p(
+            x, mu1, sigma1)))
+        dmu2 = np.sum(T1 * 1/sigma2**2 * (x - mu2))
+        dsigma2 = np.sum(T1 * (- 1/sigma2 + 1/(sigma2**3) * log_p(
+            x, mu2, sigma2)))
+        return np.asarray([dtau, dmu1, dsigma1, dmu2, dsigma2])
+    th0 = (tau, mu1, sigma1, mu2, sigma2)
+    bds = Bounds(0, 1)
+    result = minimize(L_to_minimize, th0, args=(x),
+                      jac=jac_L, tol=rtol, bounds=bds)
+    return result
+    
 
 def em_double_gauss(x, tau, mu1, sigma1, mu2, sigma2, rtol=1e-3):
     pass
--- untitled0.py
+++ untitled0.py
@@ -4,33 +4,50 @@ Created on Fri Apr  8 12:13:28 2022
 
 @author: cosbo
 """
-
 import numpy as np
-import astropy.units as u
-from astropy.coordinates import SkyCoord
-from astroquery.vizier import Vizier
 import matplotlib
 import matplotlib.pyplot as plt
+from mixfit import max_likelihood
 
-center_coord = SkyCoord('02h21m00s +57d07m42s')
-vizier = Vizier(
-    columns=['RAJ2000', 'DEJ2000', 'pmRA', 'pmDE'],
-    column_filters={'BPmag': '<16', 'pmRA': '!=', 'pmDE': '!='}, # число больше — звёзд больше
-    row_limit=10000
-)
-stars = vizier.query_region(
-    center_coord,
-    width=1.0 * u.deg,
-    height=1.0 * u.deg,
-    catalog=['I/350'],  # Gaia EDR3
-)[0]
+def t(x, tau, mu1, mu2, sigma12, sigma22):
+    tau0 = tau
+    tau1 = 1 - tau0
+    
+    T0 =(tau0/ np.sqrt(2*np.pi*sigma12))* np.exp(-0.5*(x-mu1)**2/sigma12)
+    T1 =(tau1/ np.sqrt(2*np.pi*sigma22))* np.exp(-0.5*(x-mu2)**2/sigma22)
+    
+    T_sum = T1 +T0
+    T0 = np.divide(T0, T_sum, out = np.full_like(T0, 0.5), where=T_sum!=0.0)
+    T1 = np.divide(T1, T_sum, out = np.full_like(T1, 0.5), where=T_sum!=0.0)
+    return T0, T1
 
-ra = stars['RAJ2000']._data   # прямое восхождение, аналог долготы
-dec = stars['DEJ2000']._data  # склонение, аналог широты
-x1 = (ra - ra.mean()) * np.cos(dec / 180 * np.pi) + ra.mean()
-x2 = dec
-v1 = stars['pmRA']._data
-v2 = stars['pmDE']._data
+def tht (x, tau, mu, mu2, sigma1, sigma2):
+    t0, t1 = t(x, tau, mu1, mu2, sigma12, sigma22)
+    t0_sum = np.sum(t0)
+    t1_sum = np.sum(t1)
+    tau_new = t0_sum / np.shape(x)[0]
+    mu1_new = np.sum(t0*x) / t0_sum
+    mu2_new = np.sum(t1*x) / t1_sum
+    sigma1_new = np.sum(t0*(x-mu1_new)**2)/ t0_sum
+    sigma2_new = np.sum(t1*(x-mu2_new)**2)/ t1_sum
+    return (tau_new, mu1_new, mu2_new, sigma1_new, sigma2_new)
+
+
+tau = 0.5
+mu1 = 0.1
+mu2 = 0.6
+sigma12 = 0.08
+sigma22 = 0.04
+N = 100000
+x1 = np.random.normal(mu1, sigma12, int(N*tau))
+x2 = np.random.normal(mu2, sigma22, int(N*(1-tau)))
+x = np.concatenate([x1, x2])
+tau_init = 0.5
+mu1_init = np.mean(x) - np.std(x)
+mu2_init = np.mean(x) + np.std(x)
+sigma12_init = 0.01
+sigma22_init = 0.01
+
+r = max_likelihood(x, tau_init, mu1_init, sigma12_init, mu2_init, sigma22_init)
 plt.figure()
-plt.scatter(x1, x2, s=0.25)
-
+plt.hist(x, bins = 200)
