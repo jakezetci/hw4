8200eaa1f35cf86bf5751c9209c9f78068e34cfa
jakezetci <cosb.off@gmail.com>
2022-04-21 19:21:10 +0300

    fulltask

--- mixfit.py
+++ mixfit.py
@@ -54,7 +54,6 @@ def max_likelihood(x, tau, mu1, sigma1, mu2, sigma2, rtol=1e-3):
     def L_jac(th, x):
         pass
 
-    N = x.shape[0]
     th0 = (tau, mu1, sigma1, mu2, sigma2)
     bds = Bounds(0, 1)
     result = minimize(L_to_minimize, th0, args=(x),
@@ -111,17 +110,19 @@ def em_double_cluster(x, tau1, tau2, muv, mu1, mu2, sigma02,
         sigmavec = [1/a for a in np.diag(sigma)]
         exp = np.exp(-1/2 * np.sum((x-mu) * ((x-mu) @ np.diag(sigmavec)),
                                    axis=1))
-        return exp * (np.prod(sigmavec, where=np.nonzero(sigmavec))**1/2)/ ((2 * np.pi)**(1/4)) 
-    
+        return exp * (np.prod(
+            sigmavec, where=np.nonzero(sigmavec))**1/2) / ((2 * np.pi)**(1/4))
+
     def p_normal_2d(x, mu, sigma):
-        x = np.atleast_1d(x)
-        mu = np.atleast_1d(mu)
-        n = x.shape[0]
+        x = np.atleast_2d(x)
+        mu = np.atleast_2d(mu)
+        n = np.shape(x)[0]
         mu = np.broadcast_to(mu, (n, 2))
         sigmavec = [1/a for a in np.diag(sigma)]
         exp = np.exp(-1/2 * np.sum((x-mu) * ((x-mu) @ np.diag(sigmavec)),
                                    axis=1))
-        return exp * (np.prod(sigmavec, where=np.nonzero(sigmavec))**1/2) / ((2 * np.pi)**(1/2))
+        return exp * (np.prod(
+            sigmavec, where=np.nonzero(sigmavec))**1/2) / ((2 * np.pi)**(1/2))
 
     def T(x, tau1, tau2, muv, mu1, mu2, sigma02, sigmax2, sigmav2):
         sigma = np.diag(np.array([sigmax2, sigmav2]).flat)
@@ -131,47 +132,75 @@ def em_double_cluster(x, tau1, tau2, muv, mu1, mu2, sigma02,
         mu2vec = np.asarray([mu2, muv]).flatten()
         T1 = tau1 * p_normal(x, mu1vec, sigma)
         T2 = tau2 * p_normal(x, mu2vec, sigma)
-        T3 = tau3 * p_normal_2d(x[:, 2:4], [0,0], sigma02)
+        T3 = tau3 * p_normal_2d(x[:, 2:4], [0, 0], sigma02)
         T_sum = T1 + T2 + T3
-        T1 = np.divide(T1, T_sum, out=np.full_like(T1, 0.3),
+        T1 = np.divide(T1, T_sum, out=np.full_like(T1, 0.33),
                        where=T_sum != 0.0)
-        T2 = np.divide(T2, T_sum, out=np.full_like(T2, 0.3),
+        T2 = np.divide(T2, T_sum, out=np.full_like(T2, 0.33),
                        where=T_sum != 0.0)
-        T3 = np.divide(T3, T_sum, out=np.full_like(T3, 0.3),
+        T3 = np.divide(T3, T_sum, out=np.full_like(T3, 0.33),
                        where=T_sum != 0.0)
         return T1, T2, T3
+
+    def stdsum(y, mu):
+        return (y[:, 0] - mu[:, 0])**2 + (y[:, 1] - mu[:, 1])**2
 
     def step(x, theta):
         N = x.shape[0]
         T1, T2, T3 = T(x, *theta)
         tau1 = np.sum(T1) / N
         tau2 = np.sum(T2) / N
-        tau3 = np.sum(T3) / N
-        T1_2d = np.broadcast_to(T1, (2, N)).T 
+        T1_2d = np.broadcast_to(T1, (2, N)).T
         T2_2d = np.broadcast_to(T2, (2, N)).T
         mu1 = np.sum(T1_2d * x[:, :2], axis=0) / (N * tau1)
         mu2 = np.sum(T2_2d * x[:, :2], axis=0) / (N * tau2)
-        muv = np.sum(T1_2d * x[:, 2:4] + T2_2d * x[:, 2:4], axis=0) / (N * (tau1+tau2))
+        muv = np.sum(T1_2d * x[:, 2:4] +
+                     T2_2d * x[:, 2:4], axis=0) / (N * (tau1+tau2))
         mu1_to_N = np.broadcast_to(mu1, (N, 2))
         mu2_to_N = np.broadcast_to(mu2, (N, 2))
         muv_to_N = np.broadcast_to(muv, (N, 2))
-        stdsum = lambda y, mu: (y[:, 0] - mu[:, 0])**2 + (y[:,1] - mu[:,1])**2
-        sigmax2 = np.sum(T1 * stdsum(x, mu1_to_N) + T2 * stdsum(x, mu2_to_N)) / ((np.sum(T1 + T2))*2)
-        v = np.copy(x[:,2:4])
-        A = stdsum(v, muv_to_N)
-        sigmav2 = np.sum(T1 * stdsum(v, muv_to_N) + T2 * stdsum(v, muv_to_N)) / ((
-            tau1+tau2)*N*2)
-        sigma02 = np.sum(T3 * (v[:,1]**2 + v[:,0]**2)) / (2 * 2 * np.sum(T3))
-        return (tau1, tau2, muv, mu1, mu2, [0.0, 0.0, sigma02, sigma02], [sigmax2, sigmax2],
-                [sigmav2,  sigmav2])
+        sigmax2 = np.sum(T1 * stdsum(x, mu1_to_N) +
+                         T2 * stdsum(x, mu2_to_N)) / ((np.sum(T1 + T2))*2)
+        v = x[:, 2:4]
+        sigmav2 = np.sum(T1 * stdsum(v, muv_to_N) +
+                         T2 * stdsum(v, muv_to_N)) / ((
+                             tau1+tau2)*N*2)
+        sigma02 = np.sum(T3 * (v[:, 1]**2 + v[:, 0]**2)) / (2 * 2 * np.sum(T3))
+
+        return (tau1, tau2, muv, mu1, mu2, [0.0, 0.0, sigma02, sigma02],
+                [sigmax2, sigmax2], [sigmav2,  sigmav2])
+
+    def L(x, theta):
+        def Lj(x, tau, mu, sigma):
+            n = x.shape[0]
+            m = x.shape[1]
+            mu = np.broadcast_to(mu, (n, m))
+            sigmavec = [1/a for a in np.diag(sigma)]
+            L1 = np.log(tau) - 0.5*np.log(
+                np.prod(sigmavec, where=np.nonzero(sigmavec))) - 1/2 * np.sum((x-mu) *
+                                               ((x-mu) @ np.diag(sigmavec)),
+                                               axis=1)
+            return L1
+        T1, T2, T3 = T(x, *theta)
+        tau1, tau2, muv, mu1, mu2, sigma0, sigmax, sigmav = theta
+        sigma = np.diag(np.array([sigmax, sigmav]).flat)
+        sigma02 = np.diag(np.array(sigma0[2:4]).flat)
+        L = np.sum(T1 * Lj(x, tau1, np.asarray([mu1, mu2]).flatten(), np.asarray(sigma)) +
+                   T2 * Lj(x, tau2, np.asarray([mu2, muv]).flatten(), np.asarray(sigma)) +
+                   T3 * Lj(x[:, 2:4], (1-tau1-tau2), [0,0], np.asarray(sigma02)))
+        return L
 
     theta0 = (tau1, tau2, muv, mu1, mu2, sigma02,
               sigmax2, sigmav2)
     th = step(x, theta0)
+    L_old = L(x, theta0)
     for i in range(1000):
         th_new = step(x, theta0)
+        L_new = L(x, th_new)
+        if abs(L_new - L_old) < abs(rtol*L_old):
+            break
         th = th_new
-    
+        L_old = L_new
     return th
 
 def T(x, tau1, tau2, muv, mu1, mu2, sigma02, sigmax2, sigmav2):
@@ -202,7 +231,7 @@ def T(x, tau1, tau2, muv, mu1, mu2, sigma02, sigmax2, sigmav2):
     mu2vec = np.asarray([mu2, muv]).flatten()
     T1 = tau1 * p_normal(x, mu1vec, sigma)
     T2 = tau2 * p_normal(x, mu2vec, sigma)
-    T3 = tau3 * p_normal_2d(x[:, 2:4], [0,0], sigma02)
+    T3 = tau3 * p_normal_2d(x[:, 2:4], [0, 0], sigma02)
     T_sum = T1 + T2 + T3
     T1 = np.divide(T1, T_sum, out=np.full_like(T1, 0.3),
                    where=T_sum != 0.0)
--- per.py
+++ per.py
@@ -5,14 +5,47 @@ import astropy.units as u
 from astropy.coordinates import SkyCoord
 from astroquery.vizier import Vizier
 from mixfit import em_double_cluster, T
-import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.ticker import AutoMinorLocator
+import json
+
+
+style_default = matplotlib.font_manager.FontProperties()
+style_default.set_size('large')
+style_default.set_family(['Calibri', 'Helvetica', 'Arial', 'serif'])
+style_BIG = matplotlib.font_manager.FontProperties()
+style_BIG.set_size('xx-large')
+style_BIG.set_family(['Calibri', 'Helvetica', 'Arial', 'serif'])
+
+
+def add_colorbar(mappable, side, label='probability of being in a cluster',
+                 style=style_default):
+
+    last_axes = plt.gca()
+    ax = mappable.axes
+    fig = ax.figure
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes(side, size="5%", pad=0.6)
+    if side == 'right' or side == 'left':
+        cbar = fig.colorbar(mappable, cax=cax, orientation='vertical')
+        cax.yaxis.set_ticks_position(side)
+        cbar.ax.set_ylabel(label)
+    else:
+        cbar = fig.colorbar(mappable, cax=cax, orientation='horizontal')
+        cax.xaxis.set_ticks_position(side)
+        cbar.ax.set_xlabel(label, fontproperties=style)
+    plt.sca(last_axes)
+    return cbar
+
 
 if __name__ == "__main__":
     center_coord = SkyCoord('02h21m00s +57d07m42s')
     vizier = Vizier(
         columns=['RAJ2000', 'DEJ2000', 'pmRA', 'pmDE'],
-        column_filters={'BPmag': '<16', 'pmRA': '!=', 'pmDE': '!='}, # число больше — звёзд больше
+        column_filters={'BPmag': '<16', 'pmRA': '!=', 'pmDE': '!='},
+        # число больше — звёзд больше
         row_limit=10000
     )
     stars = vizier.query_region(
@@ -29,20 +62,71 @@ if __name__ == "__main__":
     v1 = stars['pmRA']._data
     v2 = stars['pmDE']._data
 
-
     x = np.asarray([x1, x2, v1, v2]).T
-    mu = np.asarray([1, 2, 3, 4])
-    mu = np.broadcast_to(mu, (4345, 4))
-    sigmavec = [0.01, 0.01, 0.01, 0.2]
+
     theta0 = (0.2, 0.3, [-10, -2], [34.85, 57.17], [35.3, 57.17],
               [0, 0, 0.5, 0.5], [0.5, 0.5], [0.5, 0.5])
+
     r = em_double_cluster(x, *theta0)
+    param = {'tau1': r[0], 'tau2': r[1],
+             'tau3': 1 - r[0] - r[1],
+             'muv': r[2], 'mu1': r[3],
+             'mu2': r[4], 'sigma02': r[5],
+             'sigmax2': r[6], 'sigmav2': r[7]}
     T1, T2, T3 = T(x, *r)
-    
-    xx = pd.DataFrame({'x1': x1, 'x2': x2, 'value': T1+T2})
-    print(r[0]+r[1])
-    plt.figure()
-    plt.scatter(xx.x1, xx.x2, c=xx.value, cmap="Oranges")
-    plt.plot(r[3][0], r[3][1], 'o', ms=4, color='blue')
-    plt.plot(r[4][0], r[4][1], 'o', ms=4, color='blue')
-    mu1vec = np.asarray([[-10,-2], [34.85, 57.17]]).flatten()
+
+    '''Время джейсона'''
+
+    dictionary = {
+        "size_ratio": param['tau1'] + param['tau2'],
+        "motion": {"ra": param['muv'][0], "dec": param['muv'][1]},
+        "clusters": [
+            {
+                "center": {"ra": param['mu1'][0], "dec": param['mu1'][1]},
+                },
+            {
+                "center": {"ra": param['mu2'][0], "dec": param['mu2'][1]},
+                }
+            ]
+        }
+
+    with open('per.json', 'w') as f:
+        json.dump(dictionary, f, indent=2)
+
+    '''Время графиков'''
+
+    fig, (velocities, loc) = plt.subplots(1, 2, figsize=(14, 5))
+
+    dotsv = velocities.scatter(v1, v2, c=T1+T2, cmap="Oranges")
+    add_colorbar(dotsv, 'top')
+    circlev = matplotlib.patches.Circle(param['muv'],
+                                        np.sqrt(param['sigmav2'][0]),
+                                        color='orange', fill=False)
+    velocities.add_patch(circlev)
+    velocities.xaxis.set_minor_locator(AutoMinorLocator(4))
+    velocities.yaxis.set_minor_locator(AutoMinorLocator(4))
+    velocities.grid(which='minor', linestyle="--")
+    velocities.grid(which='major', linestyle=':')
+    velocities.set_xlabel('mu_right ascension', fontproperties=style_default)
+    velocities.set_ylabel('mu_declination', fontproperties=style_default)
+    velocities.set_title('Velocities distribution', fontproperties=style_BIG)
+    dotsx = loc.scatter(x1, x2, c=T1 + T2, cmap="Blues")
+    loc.plot(r[3][0], r[3][1], 'o', ms=4, color='blue')
+    loc.plot(r[4][0], r[4][1], 'o', ms=4, color='blue')
+    loc.set_xlabel('right ascension', fontproperties=style_default)
+    loc.set_ylabel('declination', fontproperties=style_default)
+    loc.set_title('Coordinates distribution', fontproperties=style_BIG)
+    circleh = matplotlib.patches.Circle(param['mu1'],
+                                        np.sqrt(param['sigmax2'][0]),
+                                        color='blue', fill=False)
+    circlex = matplotlib.patches.Circle(param['mu2'],
+                                        np.sqrt(param['sigmax2'][0]),
+                                        color='blue', fill=False)
+    loc.add_patch(circleh)
+    loc.add_patch(circlex)
+    loc.xaxis.set_minor_locator(AutoMinorLocator(4))
+    loc.yaxis.set_minor_locator(AutoMinorLocator(4))
+    loc.grid(which='minor', linestyle="--")
+    loc.grid(which='major', linestyle=':')
+    add_colorbar(dotsx, 'top')
+    fig.savefig('per.png')
